c5b86d3fd2cab8d7bb33f5141d913ade27385bce
max975 <iimax975ii@gmail.com>
2021-03-17 22:15:13 +0300

    end 2th weeks

--- stepik_tours/settings.py
+++ stepik_tours/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'hdkc#-9&6m=-3flo=$8aneym3zs%2x*-*rn2(dtmgwpnzw(_9h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost']
 
